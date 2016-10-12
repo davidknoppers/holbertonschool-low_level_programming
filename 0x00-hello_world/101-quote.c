4f900fbca7500563ad1366a141c48074856f505a
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-12 22:24:30 +0000

    Attempt 1 at 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - writes message to stderror and returns 1
+ * Return: 1 in all cases
+ */
+int main(void)
+{
+write (2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
