884bfa0e42c848ba1beeffe4c998265ac4733a5c
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-12 21:28:33 +0000

    Fixed documentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - holds all sizeof values and prints them with printf, then returns 0
+ * Return: 0 by default
+ */
 
 int main(void)
 {
@@ -7,10 +11,10 @@ int int_size = sizeof(int);
 int long_size = sizeof(long);
 int long_long_size = sizeof(long long);
 int float_size = sizeof(float);
-printf("Size of a char: %d byte(s)\n",char_size);
-printf("Size of an int: %d byte(s)\n",int_size);
-printf("Size of a long int: %d byte(s)\n",long_size);
-printf("Size of a long long int: %d byte(s)\n",long_long_size);
-printf("Size of a float: %d byte(s)\n",float_size);
- return (0);
+printf("Size of a char: %d byte(s)\n", char_size);
+printf("Size of an int: %d byte(s)\n", int_size);
+printf("Size of a long int: %d byte(s)\n", long_size);
+printf("Size of a long long int: %d byte(s)\n", long_long_size);
+printf("Size of a float: %d byte(s)\n", float_size);
+return (0);
 }
