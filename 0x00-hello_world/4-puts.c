d47f7086161421ba1695962106dac63d422424ef
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-12 21:05:19 +0000

    Revised to meet Betty style guide

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - defines a string and prints it with puts
+ * Return: 0 in all cases
+ */
 int main(void)
 {
-  char string[] = "\"Programming is like building a multilingual puzzle";
-  puts(string);
-  return 0;
+char string[] = "\"Programming is like building a multilingual puzzle";
+
+puts(string);
+return (0);
 }
