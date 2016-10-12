65d361bbb94131539defd28c7c5ea6fcd343ce6e
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-12 21:13:11 +0000

    Updating 5 to match betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints a line with printf and returns 0
+ * Return: 0 in all cases
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
