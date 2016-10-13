7265022778d03e690fb9f298ffba6226dfc8304d
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-13 22:36:43 +0000

    First serious attempt at 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,33 +6,42 @@
 
 int main(void)
 {
-int a;
-int b;
-int c;
+	int a;
+	int b;
+	int c;
+	int d;
+	int temp;
 
-for (a = 0 ; a <= 8; a++)
-{
-for (b = 1 ; b <= 9; b++)
-{
-for (c = 2 ; c <= 9; c++)
-{
-if (a < b)
-{
-if (b < c)
-{
-putchar('0' + a);
-putchar('0' + b);
-putchar('0' + c);
-if (a != 7 || b != 8 || c != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = a; c <= 9; c++)
+			{
+				if (c == a)
+				{
+				temp = b + 1;
+				}
+				else
+				{
+				temp = 0;
+				}
+				for (d = temp; d <= 9; d++)
+					{
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar(' ');
+					putchar('0' + c);
+					putchar('0' + d);
+					if (!(a == 9 && b == 8 && c == 9 && d == 9))
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
