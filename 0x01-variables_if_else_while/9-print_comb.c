5787f3af60eab8b26bc5a6d8e6d09742940f6f25
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-13 22:50:34 +0000

    Attempting to fix formatting in 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,18 @@
  * main - prints 1 through 9, in ascending order, using loops and conditionals
  * Return: 1 through 9, separated by commas
  */
-
 int main(void)
 {
-int n;
-
-for (n = 0 ; n <= 9; n++)
-{
-putchar('0' + n);
-if (n != 9)
-{
-putchar(',');
-putchar (' ');
-}
-}
-putchar('\n');
-return (0);
+	int n;
+	for (n = 0 ; n <= 9; n++)
+	{
+		putchar('0' + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
