98036840c37f8f8e61e98b4891dbf64a52c2ec3e
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:52:18 +0000

    Conformed to 10 to Betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 /**
+ * add - adds and returns numbers
+ * @a: the first int
+ * @b: the second int
  *
- *
+ * Return: sum as int
  */
 int add(int a, int b)
 {
 	int sum;
-	sum = a+b;
-	return sum;
+
+	sum = a + b;
+	return (sum);
 }
