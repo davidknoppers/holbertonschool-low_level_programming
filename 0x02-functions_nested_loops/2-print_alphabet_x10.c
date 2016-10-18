a6a7f99f0c0d7b4f7cd8eba6aeedea72a57a40ff
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:15:58 +0000

    Adding 2, unlikely to work because of linker issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,20 @@
 #include "holberton.h"
 /**
- * main - holds 
+ * print_alphabet_x10 - like 1-alphabet, but ten times
+ * print_alphabet - clone of 1-alphabet 
  * Return: the lowercase alphabet x10
  */
-int main (void)
+void print_alphabet(void)
+{
+        int chr = 'a';
+
+        for (chr = 'a'; chr <= 'z'; chr++)
+        {
+                _putchar(chr);
+        }
+        _putchar('\n');
+}
+void print_alphabet_x10(void)
 {
 	int counter = 0;
 	for (counter = 0; counter <= 9; counter ++)
