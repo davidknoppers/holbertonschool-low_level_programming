7700ad80d8f9e1b2e491e5057a45aecf99793245
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 21:33:08 +0000

    Fixed edge case in 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,7 +10,7 @@ int main(void)
 	long sum, start;
 
 	sum = 0;
-	for (start = 3; start < 1023; start++)
+	for (start = 3; start < 1024; start++)
 		if (start % 3 == 0 || start % 5 == 0)
 		{
 			sum += start;
