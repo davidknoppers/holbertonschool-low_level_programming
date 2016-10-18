897dc232f694baad68088fff0df85720ee26483f
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:54:39 +0000

    Conformed 11 to Betty style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-
+/**
+ * print_to_98 - prints increments or decrements to 98
+ * @n: the starting int
+ *
+ * Return: void
+ */
 void print_to_98(int n)
 {
 	int start;
+
 	if (n < 98)
 	{
 		for (start = n; start < 98; start++)
 		{
-			printf("%d, ",start);
+			printf("%d, ", start);
 		}
 	}
 	else if (n > 98)
 	{
 		for (start = n; start > 98; start--)
 		{
-			printf("%d, ",start);
+			printf("%d, ", start);
 		}
 	}
 	printf("98\n");
