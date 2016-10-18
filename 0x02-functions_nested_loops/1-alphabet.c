e366b4cd4a1b3d328ae7ae0c48d5c68d4dc193d2
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:12:58 +0000

    fixed Betty style and documentation for 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 /**
- * main - print lowercase alphabet
+ * print_alphabet - print lowercase alphabet
  * Return: void
  */
 void print_alphabet(void)
 {
-        int chr = 'a';
-        for (chr = 'a'; chr <= 'z'; chr++)
-        {
-                _putchar(chr);
-        }
-        _putchar('\n');
+	int chr = 'a';
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		_putchar(chr);
+	}
+	_putchar('\n');
 }
