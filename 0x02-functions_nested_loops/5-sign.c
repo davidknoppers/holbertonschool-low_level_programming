109ec390f71fbcb85652b5f9012499e04e252919
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:30:02 +0000

    Accidentally deleted 5 yesterday! Uploading now

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_sign - specifies sign of input int
+ * @n: input, an int
+ *
+ * Return: information about the number
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
