152295302d48945280892d7c606ab9e970da6350
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 20:01:35 +0000

    First draft of 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - computes and prints fibonacci numbers
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+	long next, a, b, sum;
+	int counter, start;
+
+	start = 2;
+	sum = 2;
+	counter = 35;
+	a = 1;
+	b = 2;
+	while (start < counter)
+	{
+		next = a + b;
+		if (next %2 == 0 && next < 4000000)
+		{
+			sum += next;
+		}
+		a = b;
+		b = next;
+		start++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
