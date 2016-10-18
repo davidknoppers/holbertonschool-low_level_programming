9d110631b77c2c44cc62c5fa862282874c6a6cc9
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-18 17:37:09 +0000

    Conformed 8 to Betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
-
 /**
- *
- *
+ * jack_bauer - prints very minute in Jack's long day
+ * Return: none
  */
 
 void jack_bauer(void)
 {
 	int hours, minutes;
-	for (hours = 0; hours <=23; hours++)
+
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for (minutes = 0; minutes <=59; minutes++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar(hours / 10 + 48);
 			_putchar(hours % 10 + 48);
