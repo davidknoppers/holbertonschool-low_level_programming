a9faa6506c7b8524df6f179600edea9dfd71590b
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-20 23:45:05 +0000

    uploading slow version of 100

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long n, start, best;
+	n = 612852475143;
+	best = 0;
+	start = 3;
+	while ((start * start) <= n);
+	{
+		if (n % start == 0)
+		{
+			n = n / start;
+			best = start;
+		}
+		else
+		{
+			start += 2;
+		}
+	}
+	if (n > best)
+	{
+		best = n;
+	}
+	printf("%ld\n",best);
+}
