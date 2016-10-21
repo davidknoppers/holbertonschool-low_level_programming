d7c571711f6f4e31203bfeb5c01616fce69ea19d
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-21 20:00:29 +0000

    Attempt to fix INT_MIN edge case

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,29 @@
 #include "holberton.h"
 
 /**
- * print_number - prints a number using only _putchar
+ * print_number_2 - prints a number using only _putchar
  * @n: integer to be printed
  * Return: none
  */
 
-void print_number(int n)
+void print_number_2(int n)
 {
-	if (n < 0)
+	if (n == 0)
 	{
-		_putchar('-');
+		_putchar('0');
+		return;
+	}
+	if (n > 0)
+	{
 		n *= -1;
 	}
-	if (n / 10 != 0)
+	else
+	  {
+	    _putchar('-');
+	  }
+	if ((n / 10)*-1 != 0)
 	{
-		print_number(n / 10);
+	  print_number_2((n / 10)*-1);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((n % 10)* -1 + '0');
 }
