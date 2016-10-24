30a17df7d5b37eeb2d8199dc299809c89697da64
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-24 22:05:09 +0000

    adding undocumented, unformatted draft of 7

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: a given string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
+void puts_half(char *str)
+{
+	int n, start;
+	int length;
+
+	length = _strlen(str);
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length - 1) / 2;
+	}
+        for (n = start; n < length; n++)
+        {
+                _putchar(*(str + n));
+        }
+        _putchar('\n');
+}
