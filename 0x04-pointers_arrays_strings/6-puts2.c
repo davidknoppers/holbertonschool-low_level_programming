c6cbc1f1f8b8ebfb109d312627b55d0d2d1417d7
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-24 22:00:49 +0000

    adding first unformatted draft of 6

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: a given string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
+void puts2(char *str)
+{
+	int n, end;
+	int length;
+
+	length = _strlen(str);
+	if (length % 2 == 0)
+	{
+		end = length - 2;
+	}
+	else
+	{
+		end = length - 1;
+	}
+        for (n = 0; n <= end; n++)
+        {
+		if (n % 2 == 0)
+                _putchar(*(str + n));
+        }
+        _putchar('\n');
+}
