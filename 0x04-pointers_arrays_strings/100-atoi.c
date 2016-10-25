1c6105908e59c912d8615989e00343c45ef769e0
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-25 22:21:23 +0000

    risking catastrophe to handle INT_MIN

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -19,14 +19,14 @@ int _atoi(char *s)
 		}
 		if (*(s + n) >= '0' && *(s + n) <= '9')
 		{
-			result = 10 * result + (*(s + n) - '0');
+			result = 10 * result - (*(s + n) - '0');
 		}
-		if (result > 0 && (*(s + n) < '0' || *(s + n) > '9'))
+		if (result < 0 && (*(s + n) < '0' || *(s + n) > '9'))
 		{
 			break;
 		}
 	}
-	if (sign < 0)
+	if (sign > 0)
 	{
 		result *= -1;
 	}
