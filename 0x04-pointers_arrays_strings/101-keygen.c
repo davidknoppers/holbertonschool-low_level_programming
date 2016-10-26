04d30ff41f1ddbdeb6bfdcd7b04f93f9ce61f12f
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-26 21:23:59 +0000

    First serious attempt at 101

--- 0x04-pointers_arrays_strings/101-keygen.c
+++ 0x04-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,28 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - does nothing, because I don't get decompilation
- *
- * Return: definitely void
- */
+* main - generates a funky password
+*
+* Return: 0 upon program completion
+*/
 int main(void)
 {
-	return;
+	int final, rnum, sum;
+	time_t t;
+
+	sum = 0;
+	srand((unsigned int) time(&t));
+	while (sum < 2646)
+	{
+		rnum = (rand() % 1000) / 5;
+		if (rnum >= 33 && rnum <= 126)
+		{
+			printf("%c", rnum);
+	sum += rnum;
+		}
+	}
+	final = 2772 - sum;
+	printf("%c", final);
+	return (0);
 }
