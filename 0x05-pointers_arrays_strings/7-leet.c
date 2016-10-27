dc7f9111eb8386d3682d4f74f8e07f465badeb91
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-27 22:56:42 +0000

    First l337 attempt

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,21 +1,29 @@
 #include "holberton.h"
+/**
+ * leet - l337codes a string
+ *
+ * @chr: the string to be encoded
+ * Return: the encoded string
+ */
 
 char *leet(char *chr)
 {
 	char letters[] = "oOlLeEaAtT";
 	char leets[] = "0011334477";
-	int i,j;
+	int i, j;
+
 	i = 0;
-	while (*(chr+i) != '\0')
+	while (*(chr + i) != '\0')
 {
 	j = 0;
+}
 	while (j <= 9)
 	{
 		if (*(chr + i) == letters[j])
-			       {
-				       *(chr + i) = leets[j];
-				       break;
-			       }
+		{
+			*(chr + i) = leets[j];
+			break;
+		}
 		j++;
 	}
 	i++;
