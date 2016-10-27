087dc4e0cf130f34ebd57f89d8272d32ed165461
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-27 23:28:32 +0000

    Will my old solution work??

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,28 @@
+/**
+ * print_number - my solution from 0x03
+ *
+ * @n: the number to be printed
+ * Return: the number
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n > 0)
+	{
+		n *= -1;
+	}
+	else
+	{
+		_putchar('-');
+	}
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
+}
