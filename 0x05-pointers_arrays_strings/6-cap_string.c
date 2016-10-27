8af350442e58808216b3f1b07b5b9b8f192214bc
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-27 19:37:21 +0000

    first draft of 6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes the words, with specified delimiters
+ *
+ * @chr: string to be capitalized
+ * Return: capitalized string chr
+ */
+char *cap_string(char *chr)
+{
+	int i, n;
+	if (*chr == '\0')
+	{
+		return (chr);
+	}
+	i = 1;
+	while (*(chr+i) != '\0')
+	{
+		n = *(chr + (i - 1));
+		if ((n == ',' || n == ';' || n == '.' || n == '!' || n == '?'
+		     || n == '"' || n == '(' || n == ')' || n == '{' || n == '}'
+		     || n == '\n' || n == ' ' || n == '\t')
+		    && (*(chr + i) >= 'a' && *(chr + i) <= 'z'))
+		{
+			*(chr +i) -= 32;
+		}
+		i++;
+	}
+	return (chr);
+}
