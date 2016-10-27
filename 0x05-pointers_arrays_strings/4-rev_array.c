d25693d018e3ca2fffee5c8b3b5bb4a76fd296dc
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-27 22:50:44 +0000

    fixing array reverser and docs

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
 /**
+ * reverse_array - reverses an array!
  *
- *
- *
+ * @a: pointer to array of ints
+ * @n: length of array
+ * Return: none
  */
 void reverse_array(int *a, int n)
 {
 	int temp, i, j, end;
 
-	end = n/2;
+	if (n <= 0)
+	{
+		return;
+	}
+	end = n / 2;
 	for (i = 0; i <= end; i++)
 	{
-		j = n-(i+1);
+		j = n - (i + 1);
 		temp = *(a + i);
 		*(a + i) = *(a + j);
 		*(a + j) = temp;
