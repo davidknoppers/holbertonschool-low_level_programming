1839b9046c32501527f1494de8b856211ce369c0
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-28 05:19:22 +0000

    Updating header file and adding trash for 102 and 103

---
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *
+ * nope
+ *
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * fwrite?
+ *
+ *
+ * printf%02X?
+ */
+void print_buffer(char *b, int size)
+{
+return (0);
+}
