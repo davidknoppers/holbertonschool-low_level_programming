98bf192b75d9457acc3a9f70e817c61b158eb1f3
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-31 22:06:26 +0000

    attempting to control for input mismatch in 5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,20 @@
 #include "holberton.h"
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: a given string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
 /**
  * _strstr - strings strings
  *
@@ -8,9 +24,13 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *temp;
-	char *start;
+	char *temp, *start;
+	int hay_len, ndl_len;
 
+	hay_len = _strlen(haystack);
+	ndl_len = _strlen(needle);
+	if (hay_len < ndl_len)
+		return (0);
 	temp = needle;
 	while (*haystack != '\0')
 	{
