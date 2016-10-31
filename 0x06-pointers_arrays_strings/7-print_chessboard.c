cf8af7a69d68b026b48e6d6b6e105238e3f0661e
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-31 22:13:52 +0000

    First real draft of 7

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -8,9 +8,11 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	for (i = 0; i < 8; i++)
+
+	for (i = 0; i < 8; i++, *a++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j] + '0');
+			_putchar((*a)[j]);
+		_putchar('\n');
 	}
 }
