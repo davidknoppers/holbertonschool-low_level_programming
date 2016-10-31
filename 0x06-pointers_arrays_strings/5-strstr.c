d13d841e3360a9a48923ef89ec5422cdbfa4ddf0
davidknoppers <david.knoppers@holbertonschool.com>
2016-10-31 21:23:17 +0000

    scrounging for points for 5

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -20,9 +20,9 @@ char *_strstr(char *haystack, char *needle)
 			while (*start == *needle && *start != '\0')
 			{
 				start++, needle++;
+				if (*needle == '\0')
+					return (haystack);
 			}
-			if (*needle == '\0')
-				return (haystack);
 		}
 		haystack++;
 		needle = temp;
