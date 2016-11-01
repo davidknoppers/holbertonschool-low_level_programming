e731c5225c905eb2f9c9181c39e02158921fea6e
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-01 00:38:23 +0000

    5 all day

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,21 +1,6 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- *
- * @s: a given string
- * Return: the length of the string
- */
-
-int _strlen(char *s)
-{
-	int n;
-
-	for (n = 0; *s != '\0'; s++)
-		n++;
-	return (n);
-}
-/**
- * _strstr - strings strings
+ * _strstr - it strings strings, duhhhhhh
  *
  * @haystack: string to be searched
  * @needle: string to match
@@ -24,27 +9,22 @@ int _strlen(char *s)
 char *_strstr(char *haystack, char *needle)
 {
 	char *temp, *start;
-	int hay_len, ndl_len;
 
-	hay_len = _strlen(haystack);
-	ndl_len = _strlen(needle);
-	if (hay_len < ndl_len)
-		return ('\0');
-	temp = needle;
 	while (*haystack != '\0')
 	{
 		start = haystack;
-		if (*start == *needle)
+		temp = needle;
+		if (*start == *temp)
 		{
-			while (*start == *needle && *start != '\0')
+			while (*start == *temp && *start != '\0'
+			       && *temp != '\0')
 			{
-				start++, needle++;
-				if (*needle == '\0')
-					return (haystack);
+				start++, temp++;
 			}
+			if (*temp == '\0')
+				return (haystack);
 		}
-		haystack++;
-		needle = temp;
+		haystack = start + 1;
 	}
 	return ('\0');
 }
