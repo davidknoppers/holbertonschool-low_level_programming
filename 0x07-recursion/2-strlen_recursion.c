d3bc0187638170a587f58a901a79c655cca31891
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-02 16:20:54 +0000

    First draft of 2 bc c is fun

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *  _strlen_recursion - find length of string, recursively
+ *
+ * @s: pointer to beginning of string
+ * Return: int representing string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (_strlen_recursion(s+1) + 1);
+	return (0);
+}
