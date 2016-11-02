ec3fc65150995f66ddfdc38fe01015e321c56881
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-02 17:31:02 +0000

    First draft of 6

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+int primecheck(int n,int q)
+{
+	if(q <= 1)
+		return (1);
+	else if (n % q == 0)
+		return (0);
+	else
+	{
+		return (primecheck(n, (q - 1)));
+	}
+}
+/**
+ * is_prime_number - recursively checks for prime number
+ *
+ * @n: int, potential prime
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	int start, ncopy;
+	start = n / 2;
+	ncopy = n;
+	if (n <= 1)
+		return (0);
+	if (primecheck(ncopy, start) == 1)
+		return (1);
+	else
+		return (0);
+}
