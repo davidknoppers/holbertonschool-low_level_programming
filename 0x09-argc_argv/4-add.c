d939eccbce0e15104b606399df6182a8e1d26040
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-06 20:52:53 +0000

    Removed own atoi and tested for negatives

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,37 +1,5 @@
 #include <stdio.h>
-/**
- * _atoi - converts a string to an integer
- *
- * @s: the input string
- * Return: the converted int
- */
-int _atoi(char *s)
-{
-	int n, sign, result;
-
-	sign = 1;
-	result = 0;
-	for (n = 0; *(s + n) != '\0'; n++)
-	{
-		if (*(s + n) == '-')
-		{
-			sign *= -1;
-		}
-		if (*(s + n) >= '0' && *(s + n) <= '9')
-		{
-			result = 10 * result - (*(s + n) - '0');
-		}
-		if (result < 0 && (*(s + n) < '0' || *(s + n) > '9'))
-		{
-			break;
-		}
-	}
-	if (sign > 0)
-	{
-		result *= -1;
-	}
-	return (result);
-}
+#include <stdlib.h>
 /**
  * main - sums and returns numbers, assuming proper input
  *
@@ -46,8 +14,8 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (_atoi(argv[i]))
-			sum += _atoi(argv[i]);
+		if (atoi(argv[i]) && atoi(argv[i]) > 0)
+			sum += atoi(argv[i]);
 		else
 		{
 			printf("Error\n");
