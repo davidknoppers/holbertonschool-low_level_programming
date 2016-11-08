0bbdf2dbdde5d554b92e01df438d3712b8073136
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-08 21:19:05 +0000

    beginning error handling for 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,14 +13,16 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr = (int **)malloc(width * sizeof(int *));
 
+	if (width <= 0 || height <=0)
+		return (NULL);
 	for (i = 0; i < width; i++)
 		arr[i] = (int *)malloc(height * sizeof(int));
-	if (*arr == NULL)
-		return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
 			arr[i][j] = 0;
 	}
+	if (!(arr))
+                return (NULL);
 	return (arr);
 }
