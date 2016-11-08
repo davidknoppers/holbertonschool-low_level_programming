2f14b4db8a07c186520a567ff171856be8bd143c
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-08 22:23:18 +0000

    adding potentially janky draft of 5

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - calculates and returns the length of a string
+ *
+ * @s: input, pointer to a string/char array
+ * Return: the string's length
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
+/**
+ * _strcat - uses loops and _strlen to cat two strings
+ *
+ * @src: gets copied to dest
+ * @dest: becomes the two strings combined
+ * Return: a pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int srclen = _strlen(src);
+	int destlen = _strlen(dest);
+
+	while (i <= srclen)
+	{
+		*(dest + destlen + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+/**
+ * argstostr - concatenates and prints strings from argv
+ * @ac: argc in clever disguise
+ * @av: argv[] in clever disguise
+ * Return: char
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, len;
+	char *str;
+
+	if (ac <= 1)
+		return (NULL);
+	len = 1;
+	for (i = 1; i < ac; i++)
+		len += _strlen(av[i]) + 1;
+	str = malloc(len * sizeof(char));
+	str[len] = '\0';
+	for (i = 1; i < ac; i++)
+	{
+		_strcat(str, av[i]);
+		_strcat(str, "\n");
+	}
+	if (str)
+		return (str);
+	else
+		return (NULL);
+}
