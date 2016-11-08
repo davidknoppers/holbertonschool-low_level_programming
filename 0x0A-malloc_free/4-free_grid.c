9a17bfe719147873a893440dd48d2833b651fe6d
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-08 21:25:04 +0000

    Adding short version of 4, freeing 2d array

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * free_grid - clears a two-dimensional array and makes its memory accessible
+ * @grid: grid to be cleared
+ * @height: grid height
+ * Return: none by default
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
