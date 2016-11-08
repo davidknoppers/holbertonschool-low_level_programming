c78eb6ba1810ef6b7ab73f3ff4941c1be676670d
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-08 23:39:03 +0000

    attempting to update 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr = (int **)malloc(width * sizeof(int *));
 
-	if (width <= 0 || height <=0)
+	if (width < 0 || height < 0)
 		return (NULL);
 	for (i = 0; i < width; i++)
 		arr[i] = (int *)malloc(height * sizeof(int));
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 		for (j = 0; j < height; j++)
 			arr[i][j] = 0;
 	}
-	if (!(arr))
-                return (NULL);
-	return (arr);
+	if (arr)
+		return (arr);
+	return (NULL);
 }
