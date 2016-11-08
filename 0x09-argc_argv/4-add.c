129e4edf04031ee5ee84f4b5ccd30894e4a33edd
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-08 06:28:49 +0000

    shameless rewrite

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - sums and returns numbers, assuming proper input
- *
- * @argc: num of items
- * @argv: items, as array of strings
- * Return: sum or Error
+ * main - incorporates a manual version of atoi to add nums
+ * @argc: int, number of arguments
+ * @argv: array of strings of those arguments
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int i, n, sum;
+	int i, num, result;
 
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	num = 0, result = 0;
+	i = 1;
+	if (argc <= 1)
 	{
-		n = atoi(argv[i]);
-		if (n < 0 || !atoi(argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += n;
+		printf("0\n");
+		return (0);
 	}
-	printf("%i\n", sum);
+	while (i < argc)
+	{
+		while (*argv[i] != '\0')
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+			{
+				num = num * 10 + *argv[i] - '0';
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			argv[i]++;
+		}
+		result = result + num;
+		num = 0;
+		i++;
+	}
+	printf("%d\n", result);
 	return (0);
 }
