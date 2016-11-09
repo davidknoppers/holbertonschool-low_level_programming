ca4a232d94a5b1ae0ae9cf2a5815989f7f0e5a14
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-09 17:31:33 +0000

    fixing malloc order

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -12,8 +12,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *Bob = (char *)malloc(size * sizeof(char));
+	char *Bob;
 
+	if (size > 0)
+		Bob = (char *)malloc(size * sizeof(char));
+	else
+		return (NULL);
 	for (i = 0; i <= size / sizeof(char); i++)
 		Bob[i] = c;
 	return (Bob);
