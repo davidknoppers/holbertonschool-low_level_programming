e3766b5a89c734b4bd0cc72736e162ad17d390ed
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-10 22:33:52 +0000

    changing null handling

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -27,13 +27,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len, i, j;
 	char *buff;
-	char *empty = "";
 
 	i = 0, j = 0;
 	if (s1 == NULL)
-		s1 = empty;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = empty;
+		s2 = "";
 	len = _strlen(s2);
 	if (n > len)
 		n = len;
