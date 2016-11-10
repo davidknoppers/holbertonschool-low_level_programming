b1e85e149047aba6eb9a8602d3403102cb8201c9
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-10 18:53:27 +0000

    uploading almost-correct version of 0

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - confirms that a malloc actually worked
+ *
+ * @b: pointer to memory block to be checked
+ * Return: pointer or 98, I guess?
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *testptr;
+	testptr = malloc(b);
+	if (testptr == NULL)
+		exit(98);
+	return (testptr);
+}
