3381de337c5294ef251743f3d79e99cbd2778f6b
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-15 21:15:19 +0000

    First janky attempt at fixing 4 with function reuse

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,57 @@
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: a given string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int n;
 
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
+/**
+ * _strcpy - copies and returns a string
+ *
+ * @dest: destination string, output
+ * @src: string to be copied
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+
+	n = 0;
+	while ((dest[n] = src[n]) != '\0')
+	{
+		n++;
+	}
+	return (dest);
+}
+/**
+ * _strdup - duplicates string using ye olde version of _strcpy
+ *
+ * @str: pointer to string
+ * Return: pointer to duplicated string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+
+	if (str != NULL)
+		dup = malloc(_strlen(str) + 1);
+	else
+		return (NULL);
+	if (dup == NULL)
+		return (NULL);
+	_strcpy(dup, str);
+	return (dup);
+}
 /**
  * new_dog - make instance of dog
  * @name: name of the dog
@@ -15,8 +66,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	instance = malloc(sizeof(dog_t));
 	if (instance == NULL)
 		return (NULL);
-	instance->name = name;
+	instance->name = _strdup(name);
 	instance->age = age;
-	instance->owner = owner;
+	instance->owner = _strdup(owner);
 	return (instance);
 }
