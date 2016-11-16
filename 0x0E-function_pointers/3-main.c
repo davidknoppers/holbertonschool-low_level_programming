79c9bec05f5552f2090ce19c33d693591c882947
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-16 20:05:39 +0000

    more error handling for 3

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -5,18 +5,23 @@ int main(int argc, char *argv[])
 	int a, b;
 	int (*fn)(int, int);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+        a = atoi(argv[1]);
+        b = atoi(argv[3]);
 	fn = (get_op_func(argv[2]));
 	if (fn == NULL)
 	{
 		printf("Error\n");
 		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+                exit(99);
 	}
 	printf("%d\n", fn(a, b));
 	return (0);
