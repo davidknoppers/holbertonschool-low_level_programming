7c83667fdc29f2b7f7cfb255adf825239c0239e1
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-16 17:42:04 +0000

    bad draft of 1 and updated header

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes function on elements of array
+ * @array: take a guess as to what this is
+ * @size: size of array
+ * @action: pointer to fn that takes an int and returns void
+ *
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
