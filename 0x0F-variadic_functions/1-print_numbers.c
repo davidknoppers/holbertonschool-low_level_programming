841f54f1ea5c16bce8434c17b4574d3a53bbbd43
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-19 01:28:46 +0000

    checking if the problem has to do with unsigned ints

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -17,10 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d", va_arg(ap, int));
+		printf("%d", va_arg(ap, unsigned int));
 		if (separator != NULL)
 			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(ap, int));
+	printf("%d\n", va_arg(ap, unsigned int));
 	va_end(ap);
 }
