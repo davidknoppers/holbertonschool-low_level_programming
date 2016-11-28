50fee6422700201b9c0f9d543a6aff2a4f3e6667
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-28 18:13:21 +0000

    Fixing order of includes in 0 and 1 to make compiler happy

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 /**
  * print_list - iterates through a linked list and prints its elements
  * @h: pointer to a struct, not that the compiler agrees with me
@@ -8,17 +8,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int i;
-	list_t *temp;
+	size_t i;
 
-	temp = h;
 	i = 0;
-	while (temp->str != NULL)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", temp->len, temp->str);
-		temp = temp->next;
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 		i++;
 	}
-	printf("[%d] %s\n", temp->len, temp->str);
 	return (i);
 }
