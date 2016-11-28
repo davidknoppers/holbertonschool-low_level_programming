63e8cddc3d056c462dc99befa9f4eb6b7bee08aa
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-28 21:12:15 +0000

    finally incorporated two list_ts to properly handle all(?) cases

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node_end - adds node to beginning of list
+ * @head: address of previous "first" element
+ * @str: str to dup into new node
+ * Return: address to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *temp;
+
+	new = malloc(sizeof(list_t));
+        if (new == NULL)
+                return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	temp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+        while (temp->next != NULL)
+                temp = temp->next;
+	temp->next = new;
+	temp = temp->next;
+	return (temp);
+}
