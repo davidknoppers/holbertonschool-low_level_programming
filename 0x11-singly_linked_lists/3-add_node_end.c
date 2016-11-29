8176617f85e39a4ca568e6f617428550c7a3837d
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-29 20:02:08 +0000

    attempting to remove potentially redundant line

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -41,6 +41,5 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new;
-	temp = temp->next;
-	return (temp);
+	return (temp->next);
 }
