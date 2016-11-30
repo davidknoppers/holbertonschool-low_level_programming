6b0e0ac55d1bdaa8f5f2d27da88de5a2a997c8ee
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-30 20:28:54 +0000

    First draft of 8

---
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * sum_listint - adds and returns all node values
+ * @head: ptr to head
+ * @index: index of node
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *temp;
+
+	sum = 0;
+	if (head == NULL)
+		return (sum);
+        temp = malloc(sizeof(listint_t));
+        if (temp == NULL)
+                return (sum);
+	temp = head;
+	sum += temp->n;
+        while (temp->next != NULL)
+	{
+                temp = temp->next;
+		sum += temp->n;
+	}
+	free(temp);
+	return (sum);
+}
