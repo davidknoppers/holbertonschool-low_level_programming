a43c4487c2bc40b79bc765a6c31002319dd4fa79
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-30 21:38:00 +0000

    Even less convinced than usual that this handles edge cases. Revisions to come

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - these names are getting rather verbose, eh?
+ * @head: double ptr to head
+ * @index: index at which to insert node
+ * @n: int value of the node to be inserted
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+        listint_t *new, *temp;
+        unsigned int i;
+
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+                return (NULL);
+        new->n = n;
+        new->next = NULL;
+        if (index == 0)
+        {
+                new->next = *head;
+                *head = new;
+                return (new);
+        }
+        temp = *head;
+        for (i = 0; i < index - 1; i++)
+	{
+		if (temp->next == NULL)
+			return (NULL);
+                temp = temp->next;
+	}
+        new->next = temp->next;
+        temp->next = new;
+	return (new);
+}
