03e23ebc9e9f43585709e42b21d7c6b1e750783e
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-30 19:38:51 +0000

    I want to make a Busta Rhymes reference but I can't find one where he says pop it

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - removes first node and makes head the next node
+ * @head: pointer to pointer to head
+ * Return: the integer value stored in the first node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	n = temp->n;
+	*head = (*(*head)).next;
+	free(temp);
+
+	return (n);
+}
