8096ecbef42626bd845e67d5edbb56b0c80b943d
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-30 20:10:45 +0000

    draft of 7, some edge cases still potentially a problem

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - gets a node at a given index
+ * @head: ptr to head
+ * @index: index of node
+ * Return: ptr to node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i, j;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+        temp = malloc(sizeof(listint_t));
+        if (temp == NULL)
+                return (NULL);
+	temp = head;
+	j = index;
+        for (i = 0; i < j - 1; i++)
+	{
+		if (temp->next == NULL)
+			return (NULL);
+                temp = temp->next;
+	}
+	return (temp);
+}
