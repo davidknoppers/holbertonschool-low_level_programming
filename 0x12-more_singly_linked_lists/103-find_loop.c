446460b6416e3684153d7d2a4a75c9fb909e4304
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-01 06:46:31 +0000

    not done with 101 or 102 but this problem was cooler

---
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+/**
+ * find_listint_loop - the tortoise and hare algorithm!
+ * @head: ptr to start of linked list
+ * Return: ptr to loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t  *tortoise, *hare;
+
+	if (head == NULL)
+		return (NULL);
+	tortoise = hare = head;
+	while (tortoise && hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare  = hare->next->next;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
+		}
+	}
+	return (NULL);
+}
