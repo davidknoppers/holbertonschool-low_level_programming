7c5d2c10c6bdda5f5210a9404bb4c9806af373ab
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-01 19:06:05 +0000

    added check at end if final loop terminates before reaching proper index

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 			return (NULL);
 		temp = temp->next;
 	}
+	if (i != (index - 1))
+	{
+		free(new);
+		return (NULL);
+	}
 	new->next = temp->next;
 	temp->next = new;
 	return (new);
