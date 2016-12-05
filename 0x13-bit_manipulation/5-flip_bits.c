318d435702b95c86af71800d254539fb1a1c0198
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-05 23:06:42 +0000

    fixing betty

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	count = 0;
 	temp = n ^ m;
-	for(; temp > 0; temp >>= 1)
+	for (; temp > 0; temp >>= 1)
 	{
 		if ((temp & 1) == 1)
 			count++;
