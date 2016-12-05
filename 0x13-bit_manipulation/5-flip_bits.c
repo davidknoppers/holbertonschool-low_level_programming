fcd3ac908c09890aa6d09c103ee2a9dea437a606
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-05 23:05:28 +0000

    5, flippin bits

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - at Holberton School, we flip bits
+ * @n: the first number
+ * @m: the second number, to be compared with the first
+ * Return: number of bits needed to flip from n to m or vv
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count;
+	unsigned long int temp;
+
+	count = 0;
+	temp = n ^ m;
+	for(; temp > 0; temp >>= 1)
+	{
+		if ((temp & 1) == 1)
+			count++;
+	}
+	return (count);
+}
