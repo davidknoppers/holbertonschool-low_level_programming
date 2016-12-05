0767812f7c867436b438e77f640b7a61dc388795
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-05 21:12:04 +0000

    2 is now error handled, in theory

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -8,8 +8,11 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
+	unsigned int temp;
 
+	temp = 1 << index;
+	if (temp > n)
+		return (-1);
 	bit = (n >> index) & 1;
-
 	return (bit);
 }
