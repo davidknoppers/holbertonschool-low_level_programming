cb673ab55910d015aaf756c7f46d52c51cfebfd0
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-05 22:35:54 +0000

    reasonably well tested draft of 3

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+/**
+ * set_bit - setbs bit to 1 at given index
+ * @n: input number
+ * @index: index to set to 1
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	mask = 1 << index;
+	if (index > 64)
+		return (-1);
+
+	*n = *n | mask;
+
+	return (1);
+}
