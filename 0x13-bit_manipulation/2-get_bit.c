935b43206e2933a120d5aca71b1d7d4379829ed1
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-06 04:44:29 +0000

    updating 2, hopefully

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -8,11 +8,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
-	unsigned int temp;
 
-	temp = 1 << index;
-	if (temp > n)
+	if (index > 64)
 		return (-1);
+	if (n == 0)
+		return (0);
 	bit = (n >> index) & 1;
-	return (bit);
+	if (bit)
+		return (bit);
+	else
+		return (-1);
 }
