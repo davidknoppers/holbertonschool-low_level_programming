42163e97d3f207cc2a9518de7e35ccc67c990c76
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-08 05:58:05 +0000

    fixed header issues

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,8 +1,7 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
