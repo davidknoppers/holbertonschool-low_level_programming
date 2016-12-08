b1a9dbd57bf1401f61dc540762a212e949c64f9c
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-08 18:52:16 +0000

    changed vsriables to fdr and fdw

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -12,46 +12,42 @@
 int main(int ac, char **av)
 {
 	char buffer[1204];
-	int from, to, bytes, error_check;
+	int fdr, fdw, bytes, error_check;
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
 	if (av[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 	if (av[2] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
-
-	from = open(av[1], O_RDONLY);
-	if (from == -1)
+	fdr = open(av[1], O_RDONLY);
+	if (fdr == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
-	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
-	if (to == -1)
+	fdw = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (fdw == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]), exit(99);
 
 	error_check = bytes = 1;
 	while (bytes)
 	{
-		bytes = read(from, buffer, 1204);
+		bytes = read(fdr, buffer, 1204);
 		if (bytes == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		if (bytes > 0)
 		{
-			error_check = write(to, buffer, bytes);
+			error_check = write(fdw, buffer, bytes);
 			if (error_check == -1)
 				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
 		}
 	}
-	error_check = close(from);
+	error_check = close(fdr);
 	if (error_check == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr), exit(100);
 
-	error_check = close(to);
+	error_check = close(fdw);
 	if (error_check == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw), exit(100);
 
 	return (0);
 }
