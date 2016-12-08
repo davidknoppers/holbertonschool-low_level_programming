332ab1f2cf34dd45d4a183232cf8a7cf9083b2d4
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-08 07:22:24 +0000

    totally rewrote code, removing loop and putchar and simplifying comparisons

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -2,17 +2,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  * read_textfile - reads a given number of chars from file
  * @filename: name of the file
  * @letters: number of letters to print
@@ -21,32 +10,30 @@ int _putchar(char c)
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	ssize_t i, letter_count;
+	ssize_t letter_count, temp;
 	char *buffer;
 	int file;
 
 	if (filename == NULL)
 		return (0);
-
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
 	letter_count = read(file, buffer, letters);
 	if (letter_count == -1)
 		return (0);
-	i = 0;
-	while(i < letter_count)
-	{
-		_putchar(buffer[i]);
-		i++;
-	}
-	close(file);
+	letter_count = write(STDOUT_FILENO, buffer, letter_count);
+	if (letter_count == -1)
+		return (0);
+
 	free(buffer);
 
-	return (i);
+	temp = close(file);
+	if (temp == -1)
+		return (0);
+
+	return (letter_count);
 }
