98dbfa657ca9319cc09afaf5cec5ebe41b757b92
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-08 05:55:47 +0000

    fixing up 0 quite a lot

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -1,35 +1,52 @@
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * read_textfile - reads a given number of chars from file
  * @filename: name of the file
  * @letters: number of letters to print
  * Return: number of letters successfully printed
  */
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	size_t i;
-	FILE *file;
-	int temp;
+	ssize_t i, letter_count;
+	char *buffer;
+	int file;
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-
-	if (!file)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
 
+	letter_count = read(file, buffer, letters);
+	if (letter_count == -1)
+		return (0);
 	i = 0;
-	while (i < letters && (temp = getc(file)) != EOF)
+	while(i < letter_count)
 	{
-		putchar(temp);
+		_putchar(buffer[i]);
 		i++;
 	}
-	fclose(file);
-
+	close(file);
+	free(buffer);
 	return (i);
 }
