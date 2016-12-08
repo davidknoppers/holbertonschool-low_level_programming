d15c2c526ac90bfd46065dea1dfe578410dce983
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-08 18:30:39 +0000

    fixed if statement in main while loop

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include "holberton.h"
-#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 /**
  * main - attempts to copy from file to file
  * @ac: should be 3
@@ -16,23 +16,22 @@ int main(int ac, char **av)
 	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
+
 	if (av[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	if (av[2] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
+
 	from = open(av[1], O_RDONLY);
 	if (from == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	}
+
 	to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
 	if (to == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]), exit(99);
-	}
+
 	error_check = bytes = 1;
 	while (bytes)
 	{
@@ -40,15 +39,19 @@ int main(int ac, char **av)
 		if (bytes == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		if (bytes > 0)
+		{
 			error_check = write(to, buffer, bytes);
-		if (error_check == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
+			if (error_check == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
+		}
 	}
 	error_check = close(from);
 	if (error_check == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+
 	error_check = close(to);
 	if (error_check == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
+
 	return (0);
 }
