06a4ee5552e47dae78c855625db795bdc25d3304
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-09 02:09:53 +0000

    added lots of error handling and used better variable names

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	ssize_t letter_count, temp;
+	ssize_t letter_count, write_count, error_check;
 	char *buffer;
 	int file;
 
@@ -21,20 +21,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	file = open(filename, O_RDONLY);
 	if (file == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 	letter_count = read(file, buffer, letters);
 	if (letter_count == -1)
+	{
+		free(buffer);
 		return (0);
-	letter_count = write(STDOUT_FILENO, buffer, letter_count);
-	if (letter_count == -1)
+	}
+	if (letter_count > 0)
+		write_count = write(STDOUT_FILENO, buffer, letter_count);
+	if (write_count < letter_count)
+	{
+		free(buffer);
 		return (0);
+	}
 
-
+	error_check = close(file);
+	if (error_check == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 	free(buffer);
-
-	temp = close(file);
-	if (temp == -1)
-		return (0);
-	free(buffer);
-	return (letter_count);
+	return (write_count);
 }
