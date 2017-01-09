c859e68bede01b248b35be8c545738e15c386de9
davidknoppers <david.knoppers@holbertonschool.com>
2017-01-09 07:00:31 +0000

    removing debug statement

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (temp->next == NULL)
 			return (-1);
 	}
-	printf("Attempting to remove: %d\n", temp->n);
 	if (i == index)
 	{
 		if ((*head)->next == NULL)
