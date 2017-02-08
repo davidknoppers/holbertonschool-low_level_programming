5e56f384d1039e23d947799ff6d439dcc37ddb5b
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-08 00:58:42 +0000

    program matches sample output but is likely buggy

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,40 +1,67 @@
 #include "sort.h"
+listint_t *get_node_at_index(listint_t *head, size_t index)
+{
+        size_t i;
+
+        if (head == NULL)
+                return (NULL);
+        for (i = 0; i < index && head != NULL; i++)
+        {
+                head = head->next;
+        }
+        if (i != index)
+                return (NULL);
+        return (head);
+}
+void swap_values(listint_t *left, listint_t *right)
+{
+        if (left->prev)
+                (left->prev)->next = right;
+        if (right->next)
+                (right->next)->prev = left;
+        left->next = right->next;
+        right->prev = left->prev;
+
+        left->prev = right;
+        right->next = left;
+}
+size_t listint_len(const listint_t *h)
+{
+        size_t i;
+
+        i = 0;
+        while (h != NULL)
+        {
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
 /**
  * insertion_sort_list - sorts a dll using the classic sort
  * @list: double ptr to header of dll
  */
 void insertion_sort_list(listint_t **list)
 {
-	size_t length, i, hole;
-	listint_t *right;
-	int temp;
+	size_t length, i, j;
+	listint_t *temp1, *temp2;
 
 	length = listint_len(*list);
+	temp2 = *list;
 	for (i = 1; i < length; i++)
 	{
-		temp = (get_node_at_index(*list, i))->n;
-		hole = i;
-		right = get_node_at_index(*list, hole - 1);
-		while (hole > 0 && right->n > temp)
+		temp1 = (get_node_at_index(temp2, i));
+		j = i;
+		while (j > 0 && temp1->prev && (temp1->prev)->n > temp1->n)
 		{
-			swap_values(right, right->prev);
-			hole--;
-			right = right->prev;
+			swap_values(temp1->prev, temp1);
+			j--;
+			temp2 = temp1;
+			while (temp2->prev)
+				temp2 = temp2->prev;
+			print_list(temp2);
 		}
 	}
+	while ((*list)->prev)
+		(*list) = (*list)->prev;
 }
-
-/**
-	for (i = 1; i < array_length; i++)
-	{
-		temp = array[i];
-		hole = i;
-		while (hole > 0 && array[hole - 1] > temp)
-		{
-			array[hole] = array[hole - 1];
-			hole--;
-		}
-		array[hole] = temp;
-	}
-	return (0);
-*/
