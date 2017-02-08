c6cc76326b770b4363c3f31f9f8c22215c927343
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-08 07:54:03 +0000

    recurses infinitely but nonetheless is worth saving on github

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,10 +1,48 @@
 #include "sort.h"
-int partition(int *array, size_t size)
-{}
+size_t hoare_partition(int *array, size_t right)
+{
+	size_t i, j;
+	int temp, pivot;
+
+	pivot = array[right];
+	printf("pivot is %d\n", pivot);
+
+	i = 0, j = right;
+	while (i <= j)
+	{
+		while (array[i] < pivot)
+			i++;
+		printf("i is %d\n", array[i]);
+		while (array[j] > pivot)
+			j--;
+		printf("j is %d\n", array[j]);
+		if (i <= j)
+		{
+			print_array(array, right + 1);
+			printf("a swap should occur between %d and %d\n", array[i], array[j]);
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, right + 1);
+			i++;
+			j--;
+		}
+	}
+	return (i);
+}
 
 void quick_sort(int *array, size_t size)
 {
-	if (size > 1)
-		
-	
+	size_t pivot;
+
+	static size_t right;
+
+	right = size - 1;
+
+	if (size <= 1)
+		return;
+
+	pivot = hoare_partition(array, right);
+	quick_sort(array, pivot);
+	quick_sort(array + pivot, size - pivot);
 }
