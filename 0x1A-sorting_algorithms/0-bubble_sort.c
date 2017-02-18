0885f9ce56f73514f1b0cd60bda94a2f013bd33f
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-18 22:19:42 +0000

    updating docs and bettys

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -3,6 +3,8 @@
  * bubble_sort - sorts an array using bubble sort, approximately n^2 time
  * Iterates through the entire array, then through the array -1, then -2, etc
  * Swaps integers when int on left > int on right
+ * @array: pointer to array of ints
+ * @size: size of the array
  * Return: none
  */
 void bubble_sort(int *array, size_t size)
@@ -16,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
