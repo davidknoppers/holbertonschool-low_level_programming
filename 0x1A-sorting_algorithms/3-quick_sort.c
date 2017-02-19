087712645a88b6647574a291c7961cfad2e2c66c
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-19 00:19:48 +0000

    still off by exactly one line :(((

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,12 +1,21 @@
 #include "sort.h"
-size_t hoare_partition(int *array, size_t right)
+/**
+ * hoare_partition - quicksort's heavy lifting happens here
+ * @size: is passed all over the place bc we need it for print
+ * @array: is a pointer to the start of the array
+ * @left: starts as the leftmost ele and moves
+ * @right: starts as the rightmost and also moves
+ * Return: int that will be the new pivot
+ * credit to:
+ * http://stackoverflow.com/questions/7198121/quicksort-and-hoare-partition
+ */
+int hoare_partition(int *array, int left, int right, size_t size)
 {
-	size_t i, j;
-	int temp, pivot;
+	int i, j, pivot, temp;
 
-	pivot = array[right];
-	i = 0, j = right;
-	while (i < j)
+	pivot = array[left];
+	i = left, j = right;
+	while (1)
 	{
 		while (array[i] < pivot)
 			i++;
@@ -17,26 +26,42 @@ size_t hoare_partition(int *array, size_t right)
 			temp = array[i];
 			array[i] = array[j];
 			array[j] = temp;
-			print_array(array, right + 1);
-			i++;
-			j--;
+			print_array(array, size);
+		}
+		else
+		{
+			return (j);
 		}
 	}
-	return (i);
 }
+/**
+ * qs_helper - finds the pivot and recursively calls the partition
+ * @size: is passed all over the place bc we need it for print
+ * @array: is a pointer to the start of the array
+ * @left: starts as the leftmost ele and moves
+ * @right: starts as the rightmost and also moves
+ * Return: none
+ */
+
+void qs_helper(int *array, int left, int right, size_t size)
+{
+	int pivot;
+
+	if (left < right)
+	{
+		pivot = hoare_partition(array, left, right, size);
+		qs_helper(array, left, pivot - 1, size);
+		qs_helper(array, pivot + 1, right, size);
+	}
+}
+/**
+ * quick_sort - wrapper for actual quicksort
+ * @size: is passed all over the place bc we need it for print
+ * @array: is a pointer to the start of the array
+ * Return: none
+ */
 
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot;
-
-	static size_t right;
-
-	right = size - 1;
-
-	if (size <= 1)
-		return;
-
-	pivot = hoare_partition(array, right);
-	quick_sort(array, pivot);
-	quick_sort(array + pivot, size - pivot);
+	qs_helper(array, 0, size - 1, size);
 }
