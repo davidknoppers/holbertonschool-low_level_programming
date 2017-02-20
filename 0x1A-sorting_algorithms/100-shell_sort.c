949de5298836f144ad9ddf36e0b983fa5f45632a
davidknoppers <david.knoppers@holbertonschool.com>
2017-02-20 23:09:19 +0000

    adding shell sort, first draft, efficiencies not yet uploaded

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * shell_sort - uses insertion sort on subsets of an array
+ * excellent efficiency on small and medium-sized arrays
+ * canonical implementation adapted from Sedgewick & Wayne
+ * @array: ptr to head of array
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+		{
+			for (j = i; j >= gap && array[j] < array[j - gap];
+			     j -= gap)
+			{
+				temp = array[j];
+				array[j] = array[j - gap];
+				array[j - gap] = temp;
+			}
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
+}
