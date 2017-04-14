2c3d41250559efa124d2149d9781b2c150ccd05a
davidknoppers <david.knoppers@holbertonschool.com>
2017-04-14 22:24:14 +0000

    updating header file

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -22,6 +22,25 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 /* file 0*/
 int linear_search(int *array, size_t size, int value);
 
@@ -44,5 +63,5 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /* file 106 */
-/*skiplist_t *linear_skip(skiplist_t *list, int value);*/
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
